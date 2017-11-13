7de81b58667df9e038b8d9c86372fc5fa389357d
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-11-13 13:04:09 -0800

    added contact page

--- app.js
+++ app.js
@@ -24,6 +24,31 @@ app.get('/about', (req, res) => {
     res.render('about', {title: 'About'});
 });
 
+app.get('/contact', (req, res) => {
+    res.render('contact', {title: 'Contact'});
+});
+
+app.post('/contact/send', (req, res) => {
+    const mailConfig = Object.freeze(require('./config/contact_form'));
+    const transporter = nodeMailer.createTransport(mailConfig.auth);
+
+    const mailOptions = Object.assign({}, mailConfig.options, {
+        subject: "Website Submission",
+        text: `You have a submission with the following details...\nName: ${req.body.name}\nEmail: ${req.body.email}\nMessage:\n    ${req.body.message}</li></ul>`,
+        html: `<ul>You have a submission with the following details...<li>Name: ${req.body.name}</li><li>Email: ${req.body.email}</li><li>Message: <p>${req.body.message}</p></li></ul>`
+    });
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if(err){
+            console.log(err);
+            res.redirect('/');
+        } else {
+            console.log(`Message Sent: ${info.response}`);
+            res.send(JSON.stringify(info));
+        }
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`${new Date()}: Server listening on port ${PORT}`);
 });
